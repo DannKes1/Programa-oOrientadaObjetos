64c4592f47c4facae698478f5835bd806ad1fa8a
DannKes1 <161393763+DannKes1@users.noreply.github.com>
2024-03-18 23:11:34 -0400

    Add files via upload
    Exercicio para revisar

---
+++ ClassFuncionario/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿
+
+/*Faça um programa que possui uma classe funcionário, essa classe possui os atributos:
+matrícula, nome, CPF e salário. Na classe program, crie duas instâncias dessa classe,
+lendo os valores do usuário, e ao final imprime o nome do funcionário como maior
+salário*/
+    internal class Funcionario
+    {
+    public string matricula;
+    public string nome;
+    public string cpf;
+    public double salario;
+
+    //comparando o salário do funcionário que chama o método 
+    public string MaiorSalario(Funcionario funcionario2)
+    {
+        // salairo do funcinario que chama o metodo for maior que o salario do funcionario2 
+        if (this.salario > funcionario2.salario)
+        {
+            return this.nome;
+        }
+        else if (funcionario2.salario > this.salario)
+        {
+            return funcionario2.nome;
+        }
+        else
+        {
+            return "Ambos funcionários têm o mesmo salário";
+        }
+    }
+    }
+
---
+++ ClassFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Funcionario funcionario1 = new Funcionario();
+Console.WriteLine("Cadastro de funcionário");
+Console.WriteLine("Digite a matricula do funcinario");
+funcionario1.matricula = Console.ReadLine();
+Console.WriteLine("Digite o nome do funcinario");
+funcionario1.nome = Console.ReadLine();
+Console.WriteLine("Digite o cpf do funcinario");
+funcionario1.cpf = Console.ReadLine();
+Console.WriteLine("Digite o salário do funcinario");
+funcionario1.salario = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("");
+
+Funcionario funcionario2 = new Funcionario();
+Console.WriteLine("Cadastro de funcionário");
+Console.WriteLine("Digite a matricula do funcinario");
+funcionario2.matricula = Console.ReadLine();
+Console.WriteLine("Digite o nome do funcinario");
+funcionario2.nome = Console.ReadLine();
+Console.WriteLine("Digite o cpf do funcinario");
+funcionario2.cpf = Console.ReadLine();
+Console.WriteLine("Digite o salário do funcinario");
+funcionario2.salario = Convert.ToDouble(Console.ReadLine());
+
+
+// maior salario
+string funcionarioComMaiorSalario = funcionario1.MaiorSalario(funcionario2);
+
+// Imprimir resultados
+Console.WriteLine($"O funcionário com o maior salário é: {funcionarioComMaiorSalario}");
