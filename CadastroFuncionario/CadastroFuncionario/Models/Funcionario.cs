b82f1b7d1829afce84f1787be8f60c9d550381b5
Daniel Rocha <161393763+DannKes1@users.noreply.github.com>
2024-08-14 22:26:09 -0400

    Fiz umas coisinhas, fiz conexão com banco de dados, insert, delete e list

---
+++ CadastroFuncionario/CadastroFuncionario/Models/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace CadastroFuncionario.Models
+{
+    internal class Funcionario
+    {
+        public int IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
